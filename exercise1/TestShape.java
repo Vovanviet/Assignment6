169851a164e6a05a183c23951117dd735bfd5a16
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-03-17 15:34:42 +0700

    Create TestShape.java

---
+++ exercise1/TestShape.java
@@ -0,0 +1,12 @@
+package com.java.assignment6.exercise1;
+
+public class TestShape {
+    public static void main(String[] args) {
+        Rectangle r1=new Rectangle(5,6);
+        r1.getArea();
+        r1.toString();
+        Triangle t1=new Triangle(8,15);
+        t1.getArea();
+        t1.toString();
+    }
+}
