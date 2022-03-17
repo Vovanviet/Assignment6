3fedfdfebac5fa7afaf43f24325fd15a1fae1fca
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-03-17 15:34:33 +0700

    Assignment6

---
+++ exercise2/TestMovable.java
@@ -0,0 +1,9 @@
+package com.java.assignment6.exercise2;
+
+public class TestMovable {
+    public static void main(String[] args) {
+        MovablePoint m1=new MovablePoint(5,6);
+        m1.moveDown();
+        m1.moveLeft();
+    }
+}
